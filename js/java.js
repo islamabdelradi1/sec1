6dd918bbc413c8b1ea14732b4ac7c4f87fae04e7
islam abdelradi <islamabdelradi6@gmail.com>
2022-04-27 23:13:52 +0200

    first commitsss

---
+++ js/java.js
@@ -0,0 +1,10 @@
+var btn2=document.getElementById("btn2");
+var devs3 =document.getElementById("devs3");
+devs3.addEventListener("mouseover",(eo) => {
+    btn2.style.display="block";
+})
+var btn222=document.getElementById("btn22");
+var devs333 =document.getElementById("devs33");
+devs3.addEventListener("mouseover",(eo) => {
+    btn2.style.display="block";
+})
